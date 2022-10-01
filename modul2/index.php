af1999c392fc1f40bcfb0e927b6a2e098687f0ce
TheSkibb <kfskibrek@icloud.com>
2022-10-01 16:26:00 +0200

    updated the links

--- modul2/index.php
+++ modul2/index.php
@@ -6,11 +6,11 @@
 <body>
 
 <ul>
-<li><a href="./index2_1.php">oppgave1<a></li>
-<li><a href="./index2_2.php">oppgave2<a></li>
-<li><a href="./index2_3.php">oppgave3<a></li>
-<li><a href="./index2_4.php">oppgave4<a></li>
-<li><a href="./index2_5.php">oppgave5<a></li>
+<li><a href="./index4_1.php">oppgave1<a></li>
+<li><a href="./index4_2.php">oppgave2<a></li>
+<li><a href="./index4_3.php">oppgave3<a></li>
+<li><a href="./index4_4.php">oppgave4<a></li>
+<li><a href="./index4_5.php">oppgave5<a></li>
 </ul>
 
 </body>
