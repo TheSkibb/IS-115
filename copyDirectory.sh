255d2a6164de737d7dcaaa673f5b984f4666056e
TheSkibb <kfskibrek@icloud.com>
2022-11-16 15:13:44 +0100

    copy whole directory instead of individual files

--- copyDirectory.sh
+++ copyDirectory.sh
@@ -1,6 +1,8 @@
-modul=modul8
+modul=modul10
 
 sudo rm -rf /opt/lampp/htdocs/$modul
 sudo cp -r ./$modul /opt/lampp/htdocs/$modul
+#sudo chown daemon:daemon /opt/lampp/htdocs/$modul/files/
+#sudo chown daemon:daemon /opt/lampp/htdocs/$modul/files/*
 
 echo "files copied to $path"
